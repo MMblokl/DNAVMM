46570fceaf6aad2b8abb481042acc90136ef560e
RJC100 <205520702+RJC100@users.noreply.github.com>
2026-03-22 12:14:24 +0100

    Updated unimodal_image.py
    Added a VIT transformer as another model. Changed the variable name for the BIOSCAN5M dataset. Added the extract_components function that runs the given model and extracts the specific components from the model that is given to the function.

--- unimodal_image.py
+++ unimodal_image.py
@@ -6,19 +6,23 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from datasets import load_dataset
 import requests
-from transformers import AutoImageProcessor, AutoModel
+from transformers import AutoImageProcessor, AutoModel, ViTImageProcessor, ViTModel
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the DINOV2-small processor and model
-processor = AutoImageProcessor.from_pretrained('facebook/dinov2-small')
-model = AutoModel.from_pretrained('facebook/dinov2-small')
-model = model.to(device)
-model.eval()
+processor_dinov2 = AutoImageProcessor.from_pretrained('facebook/dinov2-small')
+model_dinov2 = AutoModel.from_pretrained('facebook/dinov2-small')
+model_dinov2 = model_dinov2.to(device)
+
+# Load the Vision transformer base processor and model
+processor_vitb = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
+model_vitb = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+model_vitb = model_vitb.to(device)
 
 # Load the BIOSCAN5M dataset
-test = load_dataset("dataset.py", name="cropped_256_train", split="train", trust_remote_code=True)
-test = test.with_format("torch", device=device)
+dataset = load_dataset("dataset.py", name="cropped_256_train", split="train", trust_remote_code=True)
+dataset = dataset.with_format("torch", device=device)
 
 # Check max size of the image, pad every image to have the same width as the image with the largest width
 def colate(batch):
@@ -36,13 +40,58 @@ def colate(batch):
     return torch.stack(padded)
 
 # Batch maker handler for the dataset
-dataloader = DataLoader(test, batch_size=16, collate_fn=colate)
+dataloader = DataLoader(dataset, batch_size=16, collate_fn=colate)
 
-# Run the model
-for batch in dataloader:
-    inputs = processor(images=batch, return_tensors="pt") # Tokenizer
-    outputs = model(**inputs) # Model output
-    outputs.pooler_output # Max pooling layer output
-    last_hidden_states = outputs.last_hidden_state # Hidden state
-    last_hidden_states[:,0] # Classification token (CLS)
-    last_hidden_states.mean(dim=1) # Mean pooling
+def extract_components(dataloader, processor, model, return_types
+):
+    """
+    Extract components from the Hugging face model.
+
+    Args:
+        dataloader: PyTorch DataLoader containing the resized dataset
+        processor: HuggingFace image processor
+        model: HuggingFace model
+        return_type: Tuple containing the types of components to extract from the model ("tokenizer", "max_pool", "hidden_state", "cls", "mean_pool")
+
+    Returns:
+    """
+    # Set model to evaluation mode
+    model.eval()
+
+    # Create dict for the return types
+    return_dict = {k: [] for k in return_types}
+
+    # Run the pretrained model and extract the components
+    for batch in dataloader:
+        inputs = processor(images=batch, return_tensors="pt") # Tokenizer
+        if "tokenizer" in return_types:
+            return_dict["tokenizer"] = inputs
+
+        outputs = model(**inputs) # Model output
+        outputs.pooler_output # Max pooling layer output
+        if "max_pool" in return_types:
+            return_dict["max_pool"] = outputs.pooler_output
+
+        last_hidden_states = outputs.last_hidden_state # Hidden state
+        if "hidden_state" in return_types:
+            return_dict["hidden_state"] = last_hidden_states
+
+        last_hidden_states[:,0] # Classification token (CLS)
+        if "cls" in return_types:
+            return_dict["cls"] = last_hidden_states[:,0]
+
+        last_hidden_states.mean(dim=1) # Mean pooling
+        if "mean_pool" in return_types:
+            return_dict["mean_pool"] = last_hidden_states.mean(dim=1)
+
+        break
+    return return_dict
+
+# Extract components from the DINOV2 model
+embeddings_dino = extract_components(
+    dataloader,
+    processor_dinov2,
+    model_dinov2,
+    return_types=("tokenizer", "max_pool", "hidden_state", "cls", "mean_pool")
+)
+print(embeddings_dino)
