d2a9acfacceb911d1bef539917973d973ea1edb5
RJC100 <205520702+RJC100@users.noreply.github.com>
2026-03-24 17:21:55 +0100

    Update unimodal_image.py
    Update the unimodal_image.py script by adding the class head for classification using the DINOV2 encoder for training the model.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DNAVMM
 Multi-modal DNA and vision species classification model for Multimodal Models in Ecology and Biodiversity
+
+# unimodal_image.py
+Obtain the dataset.py script from BIOSCAN5M to download the dataset: wget -P https://huggingface.co/datasets/bioscan-ml/BIOSCAN-5M/resolve/main/dataset.py
+
+pip install datasets==3.6.0
--- unimodal_image.py
+++ unimodal_image.py
@@ -1,31 +1,59 @@
-# wget -P https://huggingface.co/datasets/bioscan-ml/BIOSCAN-5M/resolve/main/dataset.py
-# pip install datasets==3.6.0
-
 import torch
+from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from datasets import load_dataset
-import requests
-from transformers import AutoImageProcessor, AutoModel, ViTImageProcessor, ViTModel
+from transformers import AutoModel
+import numpy as np
 
+global device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load the DINOV2-small processor and model
-processor_dinov2 = AutoImageProcessor.from_pretrained('facebook/dinov2-small')
-model_dinov2 = AutoModel.from_pretrained('facebook/dinov2-small')
-model_dinov2 = model_dinov2.to(device)
+class VisualEncoder(nn.Module):
+    def __init__(self, visual_encoder, n_classes, lr):
+        super(VisualEncoder, self).__init__()
+        self.visual_encoder = visual_encoder
+        visual_encoder_size = 384
 
-# Load the Vision transformer base processor and model
-processor_vitb = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
-model_vitb = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
-model_vitb = model_vitb.to(device)
+        # Number of classes and create np for one hot encoding
+        self.n_classes = n_classes
+        self.i = np.eye(n_classes)
 
-# Load the BIOSCAN5M dataset
-dataset = load_dataset("dataset.py", name="cropped_256_train", split="train", trust_remote_code=True)
-dataset = dataset.with_format("torch", device=device)
+        # Fully connected classification head
+        self.class_head = nn.Linear(visual_encoder_size, n_classes)
+        self.dropout = nn.Dropout(0.1)
 
-# Check max size of the image, pad every image to have the same width as the image with the largest width
-def colate(batch):
+        self.optimzer = torch.optim.Adam(self.parameters(), lr=lr)
+        self.criterion = nn.CrossEntropyLoss()
+
+def forward(self, images):
+    # CLS for embedding
+    embedding = self.visual_encoder(images).last_hidden_state[:,0]
+
+    # Pass the embedding through the model
+    logits = self.class_head(embedding)
+
+    return logits
+
+def train(self, dataloader, epochs):
+    self.optimizer.zero_grad()
+
+    for i in range(epochs):
+        self.visual_encoder.train()
+
+        for batch in dataloader:
+            images = batch["images"]
+            labels = batch["labels"]
+            labels_onehot = torch.tensor(self.i[labels])
+
+            logits = self.forward(images=images)
+            loss = self.criterion(logits, labels_onehot)
+
+            loss.backward()
+
+            self.optimizer.step()
+
+def collate(batch):
     """Custom collation function for dataloader, extract images from the batch and pad them with the largest width from the widest image.
 
     Args:
@@ -34,64 +62,29 @@ def colate(batch):
     Returns:
         torch.tensor: The stacked tensor of the batch of images.
     """
-    batch  = [i["image"] for i in batch]
-    max_width = max(img.shape[-1] for img in batch)
-    padded = [F.pad(img, (0, max_width-img.shape[-1], 0, 0)) for img in batch]
-    return torch.stack(padded)
+    labels = [species_dict[i["species"]] for i in batch]
+    images = [i["image"] for i in batch]
+    max_width = max(img.shape[-1] for img in images)
+    padded = [F.pad(img, (0, max_width-img.shape[-1], 0, 0)) for img in images]
+    return {"images": torch.stack(padded), "labels": torch.Tensor(labels).to(torch.uint16)}
 
-# Batch maker handler for the dataset
-dataloader = DataLoader(dataset, batch_size=16, collate_fn=colate)
 
-def extract_components(dataloader, processor, model, return_types
-):
-    """
-    Extract components from the Hugging face model.
+if __name__ == "__main__":
+    # Load the BIOSCAN5M dataset
+    dataset = load_dataset("dataset.py", name="cropped_256_train", split="train", trust_remote_code=True)
+    dataset = dataset.with_format("torch", device=device)
 
-    Args:
-        dataloader: PyTorch DataLoader containing the resized dataset
-        processor: HuggingFace image processor
-        model: HuggingFace model
-        return_type: Tuple containing the types of components to extract from the model ("tokenizer", "max_pool", "hidden_state", "cls", "mean_pool")
+    # Initialize the species labels
+    global species_dict
+    uniq_species = set(dataset["species"])
+    n_classes = len(uniq_species)
+    species_dict = {entry: i for i, entry in enumerate(uniq_species)}
 
-    Returns:
-    """
-    # Set model to evaluation mode
-    model.eval()
+    visual_encoder = AutoModel.from_pretrained('facebook/dinov2-small')
+    
+    model = VisualEncoder(visual_encoder, n_classes, 0.5)
+    model.to(device)
 
-    # Create dict for the return types
-    return_dict = {k: [] for k in return_types}
+    dataloader = DataLoader(dataset, batch_size=64, collate_fn=collate)
 
-    # Run the pretrained model and extract the components
-    for batch in dataloader:
-        inputs = processor(images=batch, return_tensors="pt") # Tokenizer
-        if "tokenizer" in return_types:
-            return_dict["tokenizer"] = inputs
-
-        outputs = model(**inputs) # Model output
-        outputs.pooler_output # Max pooling layer output
-        if "max_pool" in return_types:
-            return_dict["max_pool"] = outputs.pooler_output
-
-        last_hidden_states = outputs.last_hidden_state # Hidden state
-        if "hidden_state" in return_types:
-            return_dict["hidden_state"] = last_hidden_states
-
-        last_hidden_states[:,0] # Classification token (CLS)
-        if "cls" in return_types:
-            return_dict["cls"] = last_hidden_states[:,0]
-
-        last_hidden_states.mean(dim=1) # Mean pooling
-        if "mean_pool" in return_types:
-            return_dict["mean_pool"] = last_hidden_states.mean(dim=1)
-
-        break
-    return return_dict
-
-# Extract components from the DINOV2 model
-embeddings_dino = extract_components(
-    dataloader,
-    processor_dinov2,
-    model_dinov2,
-    return_types=("tokenizer", "max_pool", "hidden_state", "cls", "mean_pool")
-)
-print(embeddings_dino)
+    model.train(dataloader, epochs=3)
