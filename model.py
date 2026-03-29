bb0c06d86419c9fc9395a3fb3d02ed5fee0c7c56
MMblokl <mats@familieblokland.nl>
2026-03-29 15:05:10 +0200

    No more dataloader, index shuffling extraction

--- model.py
+++ model.py
@@ -2,19 +2,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 from torch.utils.data import DataLoader
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModel, AutoTokenizer, set_seed
 from datasets import load_dataset
 import numpy as np
+import random
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 apitoken = os.getenv("API_KEY")
 
-
 global device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 class DNAVMM(nn.Module):
     def __init__(self, d_enc, v_enc, d_tokenizer, n_classes, lr):
@@ -33,7 +32,11 @@ class DNAVMM(nn.Module):
 
         self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         self.criterion = nn.CrossEntropyLoss()
-    
+
+        # Training metrics storage
+        self.train_loss = {}
+
+
     # Untested, probably works
     def forward(self, images, dna):
         # Other options
@@ -54,11 +57,19 @@ class DNAVMM(nn.Module):
     
     # Might not be possible in same class, check RL implementations
     # Ideally, just doing model.train() will do it.
-    def fit(self, dataloader, epochs):
+    def fit(self, dataset, epochs, steps_per_epoch=100, batch_size=8):
         self.train() # Turn on dropouts
         # Loop through each epoch
-        for i in range(epochs):
-            for batch in dataloader:
+        for epoch in range(epochs):
+            self.train_loss[epoch] = []
+            
+            # Shuffle the dataset at the start for more variable data training
+            dataset = dataset.shuffle()
+            prev = 0
+            for timestep, idx in enumerate(range(batch_size, len(dataset), batch_size)):
+                # Collate data properly
+                batch = collate_fn(dataset[prev:idx])
+
                 self.optimizer.zero_grad() # Zero out previous grad
 
                 # Single out data
@@ -75,7 +86,15 @@ class DNAVMM(nn.Module):
                 loss.backward()
 
                 self.optimizer.step()
-                print(loss.item())
+                
+                # Save the loss to storage
+                self.train_loss[epoch].append(loss.item())
+                if timestep == steps_per_epoch:
+                    break
+                
+                # reset prev
+                prev = idx
+            breakpoint()
 
     
     def save(self, path):
@@ -95,16 +114,26 @@ def collate_fn(batch):
     Returns:
         torch.tensor: The stacked tensor of the batch of images.
     """
-    labels = [species_dict[i["species"]] for i in batch]
-    images = [i["image"] for i in batch]
-    barcodes = [" ".join([inp["dna_barcode"][i:i+k] for i in range(len(inp["dna_barcode"]) - k + 1)]) for inp in batch]
+    images = batch["image"]
+    labels = [species_dict[i] for i in batch["species"]]
+    barcodes = [" ".join([seq[i:i+k] for i in range(len(seq) - k + 1)]) for seq in batch["dna_barcode"]]
     max_width = max(img.shape[-1] for img in images)
     padded = [F.pad(img, (0, max_width-img.shape[-1], 0, 0)) for img in images]
-    return {"images": torch.stack(padded).to(device), "labels": torch.tensor(labels).long().to(device), "barcodes": barcodes}
+    return {"images": torch.stack(padded), "labels": torch.tensor(labels).long().to(device), "barcodes": barcodes}
     
 
 
 if __name__ == "__main__":
+    # Set seed for everything
+    seed = 202667
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    set_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
     dataset = load_dataset(
         "dataset.py",
         name="cropped_256_train",
@@ -145,12 +174,8 @@ if __name__ == "__main__":
     orig_pos_emb = d_enc.base_model.embeddings.position_embeddings.weight
     d_enc.base_model.embeddings.position_embeddings.weight = torch.nn.Parameter(torch.cat((orig_pos_emb, orig_pos_emb)))
 
-
     model = DNAVMM(d_enc, v_enc, d_tokenizer, n_classes, 1e-4)
     model.to(device)
 
-    dataloader = DataLoader(dataset, batch_size=8, collate_fn=collate_fn)
-
-    model.fit(dataloader, epochs=1)
+    model.fit(dataset, epochs=2, steps_per_epoch=20, batch_size=8)
     model.save("/local/mmeb_s4501888/model.weights")
-
