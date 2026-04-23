cee0fc82fda1b976e99968a954973e73ba4d7726
MMblokl <157376683+MMblokl@users.noreply.github.com>
2026-04-23 21:27:21 +0200

    Fixed cache (#24)
    Of course cache_dir will not magically create itself, so we do that now I am so stupid.

--- unimodal_dna.py
+++ unimodal_dna.py
@@ -175,8 +175,8 @@ if __name__ == "__main__":
     
     # If cache needs to be used
     cache_dir = os.getenv("cache_dir", default=None)
-    if cache_dir:
-        cache_dir = cache_dir if os.path.isdir(cache_dir) else None
+    if not os.path.isdir(cache_dir):
+        os.mkdir(cache_dir)
     class_indices_path = os.getenv("class_indices", default="./class_indices/")
 
     # Train dataset
